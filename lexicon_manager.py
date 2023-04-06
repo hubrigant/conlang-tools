29a93fc8b307c1a6b0c4cc8e8750e3f80fcc75dd
Jon Williams <jonhatesspam+gitrepo@gmail.com>
2023-04-06 15:34:32 -0500

    building out methods in manager class, starting to build the lexicon class, and the associated unit tests

--- lexicon_manager.py
+++ lexicon_manager.py
@@ -2,4 +2,26 @@
 
 class Manager:
     def __init__(self):
+        self.lexicon = Lexicon()
         return
+
+    def read_file(self, filename):
+        with open(filename, 'r') as fh:
+            x = fh.readline()
+        return x
+
+class Lexicon:
+    def __init__(self):
+        self._words = []
+        self._current_word = 0
+        return
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self._words) != 0 and self._current_word <= len(self._words):
+            self._current_word += 1
+            return self._words[self._current_word]
+        else:
+            raise StopIteration
